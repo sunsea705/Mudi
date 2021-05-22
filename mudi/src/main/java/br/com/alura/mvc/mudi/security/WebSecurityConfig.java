fe7b740795ccfb14cb391c5c62956a5646c439ee
Ítalo Tiago <theitalotiago@gmail.com>
2021-05-22 11:02:26 -0300

    Criação de usuários no banco com senha criptografada.

--- mudi/src/main/java/br/com/alura/mvc/mudi/security/WebSecurityConfig.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/security/WebSecurityConfig.java
@@ -1,19 +1,26 @@
 package br.com.alura.mvc.mudi.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-@SuppressWarnings("deprecation")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -34,16 +41,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .logout().logoutUrl("/logout");
 	}
 	
-	@Bean
 	@Override
-	protected UserDetailsService userDetailsService() {	 
-		System.out.println("AAA");
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("root")
-				.password("root")
-				.roles("ADM")
-				.build();
-		System.out.println("bbb");
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
